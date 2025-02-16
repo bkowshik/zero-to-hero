57a09f4688269bc77a3d22cf3770da2a462fd035
Bhargav Kowshik <bhargav.kowshik@gmail.com>
2025-02-16 17:35:32 +0530

    Using stylesheet from react native instead of inline styles

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { Button, Text, View, Image, ScrollView, TextInput, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.contentContainer}>
+      <View style={styles.container}>
+        <Text>Please fill the form below and hit submit.</Text>
+        <TextInput placeholder="Enter some text" style={{ width: 200, height: 40, margin: 10 }} />
+        <Button title="Submit" onPress={() => alert('Form submitted successfully!')} />
+        <Image source={require('@/assets/images/react-logo.png')} />
+      </View>
+    </ScrollView >
   );
 }
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+  },
+  container: {
+    backgroundColor: '#160225',
+    flex: 1,
+  }
+});
